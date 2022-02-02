2425296317db0609de67648be31dc9f75a7be9a9
ukasz1 <lukaszmitkowski53@gmail.com>
2022-02-02 09:48:04 +0100

    column charts added

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -38,28 +38,33 @@ const Repos = () => {
     })
     .slice(0, 5);
 
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13",
+  //stars, forks
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
     },
     {
-      label: "CSS",
-      value: "160",
-    },
-    {
-      label: "Javascript",
-      value: "80",
-    },
-  ];
+      stars: {},
+      forks: {},
+    }
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
+  console.log(stars);
 
   return (
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <div></div>
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
-        <div></div>
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
